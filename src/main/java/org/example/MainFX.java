5b8756c6457252954d3ebeb81203723830e6c168
yuna <oueslatiameni48@gmail.com>
2026-02-21 16:02:52 +0100

    changer de VBox vers HBox

--- src/main/java/org/example/MainFX.java
+++ src/main/java/org/example/MainFX.java
@@ -3,7 +3,7 @@ package org.example;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.TabPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class MainFX extends Application {
@@ -12,10 +12,12 @@ public class MainFX extends Application {
     public void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/service-view.fxml"));
-            TabPane root = loader.load();
-            Scene scene = new Scene(root, 1200, 700);
+            HBox root = loader.load();
+            Scene scene = new Scene(root, 1300, 750);
+
             scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
-            primaryStage.setTitle("Gestion des Services");
+
+            primaryStage.setTitle("stratiX - Gestion des Services");
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (Exception e) {
