c209affc70ebc54cca9bdbac747e2bb13787faa8
islem <islem.alibii@gmail.com>
2026-02-23 04:35:45 +0100

    mainFxFIX

--- src/main/java/org/example/MainFx.java
+++ src/main/java/org/example/MainFx.java
@@ -3,9 +3,11 @@ package org.example;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
 import javafx.stage.Stage;
 
-public class MainFx extends Application {
+public class MainFX extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
